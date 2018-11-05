8c44d751a4e49c9e8d77821d5fc0523204ecd6ad
ahmadyassinkhoja <ahmad.mohammad.khoja@gmail.com>
2018-11-05 10:14:23 +0200

    updating structure

--- directives-pipes-animation/src/app/animations/animations.component.ts
+++ directives-pipes-animation/src/app/animations/animations.component.ts
@@ -1,22 +1,9 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  // ...
-} from '@angular/animations';
-
 @Component({
   selector: 'app-animations',
   templateUrl: './animations.component.html',
   styleUrls: ['./animations.component.css'],
-  // add the animations to component's meta data
-  animations: [
-    // animation triggers go here
-  ]
 })
 export class AnimationsComponent implements OnInit {
 
